683521d031e9181afc109fff5740104ab4675d0c
sushmitha1327 <sushmithakurakulaks@gmail.com>
2025-07-22 22:31:24 +0530

    studentmanagementproject

---
+++ StudentManagementProject/src/student_app/Student.java
@@ -0,0 +1,11 @@
+package student_app;
+
+public class Student {
+		String name;
+	    int age;
+
+	    void display() {
+	        System.out.println("Name: " + name);
+	        System.out.println("Age: " + age);
+	    }
+	}
---
+++ StudentManagementProject/src/student_app/StudentTest.java
@@ -0,0 +1,11 @@
+package student_app;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		       Student s1 = new Student();
+		       s1.name = "Ayush_Wipro";
+		       s1.age = 18;
+		       s1.display();
+		   }
+		}

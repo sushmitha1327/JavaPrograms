5752bbfc9672db07913200b8b75dd2db509c354f
sushmitha1327 <sushmithakurakulaks@gmail.com>
2025-07-22 22:23:19 +0530

    Wiprojava

---
+++ WiproJavaProject/src/Wipro_java/Keyword_addition.java
@@ -0,0 +1,16 @@
+package Wipro_java;
+import java.util.Scanner;
+
+public class Keyword_addition {
+
+	public static void main(String[] args) {
+		  Scanner sc = new Scanner(System.in);   
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();   
+	        System.out.print("Enter second number: ");
+	        int b = sc.nextInt();   
+	        int sum = a + b;        
+	        System.out.println("Sum = " + sum);
+	}
+
+}

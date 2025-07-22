c60925a0bf7a6ec6f2bb73f9c8df99c20b11d990
sushmitha1327 <sushmithakurakulaks@gmail.com>
2025-07-22 22:43:20 +0530

    primitivedatatypedemo

---
+++ PrimitiveDataTypesDemo/src/datatype_demo/PrimitiveDemo.java
@@ -0,0 +1,25 @@
+package datatype_demo;
+
+public class PrimitiveDemo {
+
+	public static void main(String[] args) {
+		   byte b = 100;
+	        short s = 30000;
+	        int i = 1_000_000;
+	        long l = 5_000_000_000L;
+	        float f = 3.14f;
+	        double d = 2.71828;
+	        char c = 'Z';
+	        boolean bool = true;
+
+	        System.out.println("byte: " + b);
+	        System.out.println("short: " + s);
+	        System.out.println("int: " + i);
+	        System.out.println("long: " + l);
+	        System.out.println("float: " + f);
+	        System.out.println("double: " + d);
+	        System.out.println("char: " + c);
+	        System.out.println("boolean: " + bool);
+	    }
+	}
+

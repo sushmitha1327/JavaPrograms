11d7d949944010c59185447455dc887f84909545
sushmitha1327 <sushmithakurakulaks@gmail.com>
2025-07-22 22:12:14 +0530

    second commit

---
+++ ReferenceProject/src/reference_variables/ReferenceVariables.java
@@ -0,0 +1,26 @@
+package reference_variables;
+class Person {
+    String name;
+    Person(String name) { this.name = name; }
+    public String toString() { return name; }
+}
+
+public class ReferenceVariables {
+	public static void changeName(Person p) {
+        p.name = "Changed";
+    }
+	public static void main(String[] args) {
+		  int x = 10;
+	        int y = x;
+	        y = 20;
+	        System.out.println("x = " + x); // x stays 10 (primitive)
+
+	        Person p1 = new Person("ayz");
+	        Person p2 = p1;
+	        p2.name = "abc";
+	        System.out.println("p1 = " + p1); // abc
+
+	        changeName(p1);
+	        System.out.println("p1 after changeName = " + p1); // Changed
+	    }
+	}
